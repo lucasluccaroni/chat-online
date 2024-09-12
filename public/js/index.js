e8b7e1f3d770f1233fc9792c7ba0d1988f912092
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2024-09-12 20:53:37 -0300

    mejorando modal de sweetAlert para el ingreso de usuarios

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
 // Javascript desde el browser
-
 const socket = io()
+let user
+let chatBox = document.getElementById("chatBox")
+
+// bloquear pantalla del usuario y pedirle username
+Swal.fire({
+    title: "¡Bienvenido! Identificate.",
+    text: "Ingresa tu usuario para identificarte en el chat.",
+    input: "text",
+    allowOutsideClick: false,
+    inputValidator: (value) => {
+        if (!value) {
+            return "¡Necesitas escribir un nombre de usuario para continuar!"
+        }
+
+        const regex = /^[a-zA-Z]{3,}[0-9]*$/
+        if (!regex.test(value)) {
+            return "El nombre de usuario debe contener al menos 3 letras."
+        }
+
+        return null
+        // return !value && "¡Necesitas escribir un nombre de usuario para continuar!"
+    }
+
+}).then(result => {
+    if(result.isConfirmed) {
+        console.log(`Usuario ingresado: ${result.value}`)
+        user = result.value
+    }
+})
