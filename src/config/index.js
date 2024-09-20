2f72ec822be7694f0efa8748181f798cb43bc0e0
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2024-09-20 12:27:55 -0300

    actualizando variables de entorno

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,6 @@ const { config } = require("dotenv")
 config()
 
 module.exports = {
-    secret: process.env.SECRET,
     dbName: process.env.DB_NAME,
     port: process.env.PORT,
     mongoUri: process.env.MONGO_URI
